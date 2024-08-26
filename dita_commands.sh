c2b943ade091e7765453e7342f63a362e606754c
sai-teja2 <128804731+sai-teja2@users.noreply.github.com>
2024-08-26 19:30:43 +0530

    Update dita_commands.sh

--- dita_commands.sh
+++ dita_commands.sh
@@ -1,1 +1,7 @@
+#!/bin/bash
+export PATH=/root/dita-ot-4.2.3/bin:$PATH 
+export DITA_HOME=/root/dita-ot-4.2.3
+echo $PATH
+echo $DITA_HOME
+dita --version
 dita -i $file_name -f $format -o $out_path
