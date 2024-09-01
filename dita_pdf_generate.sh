b683ff06128b58048ed37515891d6e9108fe6dbd
sai-teja2 <128804731+sai-teja2@users.noreply.github.com>
2024-09-01 14:25:43 +0530

    Update and rename dita_commands.sh to dita_pdf_generate.sh

--- dita_pdf_generate.sh
+++ dita_pdf_generate.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 export PATH=/root/dita-ot-4.2.3/bin:$PATH 
 export DITA_HOME=/root/dita-ot-4.2.3
-echo $PATH
-echo $DITA_HOME
-dita --version
 dita -i $file_name -f $format -o $out_path
