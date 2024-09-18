3c8064fe677aaeecfa9dafc92f3e1fbbf89813b3
sai-teja2 <128804731+sai-teja2@users.noreply.github.com>
2024-09-18 14:38:46 +0530

    Update dita_pdf_map.sh

--- dita_pdf_map.sh
+++ dita_pdf_map.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 export PATH=/root/dita-ot-4.2.3/bin:$PATH 
 export DITA_HOME=/root/dita-ot-4.2.3
-dita -i $file_name.ditamap -f $format -o $out_path -filter $dita_val_name.ditval 
+dita -i $file_name.ditamap -f $format -o $out_path -filter $dita_val_name.ditaval 
